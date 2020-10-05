e2e1d3750a9ad2311d174c61eaf9063c728d465a
ebubekir dinç <Ebubekirdinc@gmail.com>
2020-10-05 09:18:44 +0300

    Projects Tests added

---
+++ test/ProgecDo.Web.Tests/Pages/Projects/ProjectsTests.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
+using ProgecDo.Projects;
+using ProgecDo.Web.Pages;
+using Shouldly;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Json;
+using Xunit;
+
+namespace ProgecDo.Pages.Projects
+{ 
+    public class FakeUserClaims
+    {
+        public List<Claim> Claims { get; } = new List<Claim>();
+    }
+
+    public class ProjectsTests : ProgecDoWebTestBase
+    {
+        private readonly IProjectAppService _projectAppService;
+
+        private readonly IJsonSerializer _jsonSerializer;
+        // private readonly IMapper _mapper;
+        // private readonly IObjectMapper _mapper;
+        // private readonly SignInManager<AppUser> _signInManager;
+        // private readonly IUserStore<AppUser> _userStore;
+
+
+        public ProjectsTests()
+        {
+            _projectAppService = GetRequiredService<IProjectAppService>();
+            _jsonSerializer = GetRequiredService<IJsonSerializer>(); 
+        }
+
+        [Fact]
+        public async Task Should_Get_CreateModal()
+        {
+            // Act 
+            var response = await Client.GetAsync("/Projects/CreateModal");
+
+            // Assert
+            response.ShouldNotBeNull();
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Should_Get_EditModal()
+        {
+            // Arrange
+            var project = await _projectAppService.GetListAsync(new PagedAndSortedResultRequestDto());
+            
+            // Act 
+            var response = await Client.GetAsync("/Projects/EditModal?id=" + project.Items.FirstOrDefault()?.Id);
+
+            // Assert
+            response.ShouldNotBeNull();
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Should_Get_IndexPage()
+        {
+            // Act 
+            var response = await Client.GetAsync("/");
+
+            // Assert
+            response.ShouldNotBeNull();
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
+        
+        [Fact]
+        public async Task Should_Get_ProjectsViewComponent()
+        {
+            // Arrange
+            var httpContext = new DefaultHttpContext();
+            var modelState = new ModelStateDictionary();
+            var actionContext = new ActionContext(httpContext, new RouteData(), new PageActionDescriptor(), modelState);
+            var pageContext = new PageContext(actionContext); 
+                
+            // Act 
+            var pageModel = new IndexModel()
+            {
+                PageContext = pageContext, 
+                Url = new UrlHelper(actionContext)
+            };
+            var result =  pageModel.OnGetProjectsViewComponent() as ViewComponentResult;
+                
+            // Assert
+            result.ShouldNotBeNull();
+            result.ShouldBeOfType<ViewComponentResult>(); 
+        }
+   
+ 
+    }
+}
